04b9091b4ef78373e68a94b52968adaeed366b26
ahmedelsewailky <ahmed.elsewailky@gmail.com>
2024-05-21 10:44:32 +0300

    Completing homepage

--- js/main.js
+++ js/main.js
@@ -35,4 +35,17 @@ $(function () {
         slidesToShow: 5,
         slidesToScroll: 1,
     });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 300) {
+            $('.back-to-top').addClass('show');
+        } else {
+            $('.back-to-top').removeClass('show');
+        }
+    });
+
+    $('.back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, '300');
+    });
 });
